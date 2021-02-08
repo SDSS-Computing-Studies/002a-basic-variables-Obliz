45da4277c2afd428198c9d9c12582a940968eb5d
Obliz <owen.wm.thomas@gmail.com>
2021-02-08 09:57:25 -0800

    Owen Thomas

---
+++ assignment2.py
@@ -0,0 +1,10 @@
+#! python 3
+
+fnname = "Owen"
+space = " "
+mdname = "William Miller"
+lsname = "Thomas"
+
+name = fnname + space + mdname + space + lsname
+
+print(name)
---
+++ assignment3.py
@@ -0,0 +1,4 @@
+#! python 3
+
+value1 = 10
+Value2 = "10"
